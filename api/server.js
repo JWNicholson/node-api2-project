6ad3da2fe0b308835f12002861b0650c938bf8b7
JW Nicholson <john.nicholson812@gmail.com>
2020-04-07 16:52:59 -0400

    added postRouter.js

--- api/server.js
+++ api/server.js
@@ -3,12 +3,12 @@ const express = require('express');
 const server = express();
 const postRouter = require('../posts/post-router.js');
 
-server.use(express.json());
 
+server.use(express.json());
 server.use('/api/posts', postRouter);
 
 server.get('/', (req,res) => {
-    res.status(200).json({message: 'server running'})
-})
+    res.status(200).json({message: 'server is running (200)'});
+});
 
 module.exports = server; 
